e6c561aec2258c24072aa2945a120ae2baea4347
Dapinder Singh <dhillon.dapinder@gmail.com>
2017-08-29 13:04:29 +0530

    Test case to exclude Pretty Jackson conf.

--- src/test/java/com/ds/web/crawler/test/config/TestConfig.java
+++ src/test/java/com/ds/web/crawler/test/config/TestConfig.java
@@ -6,12 +6,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 import com.ds.web.crawler.WebCrawlerController;
+import com.ds.web.crawler.config.CustomWebMvcConfiguration;
 import com.ds.web.crawler.config.InitalizeThreadPoolExecutor;
 
 @Configuration
 @ComponentScan(basePackages = {
 		"com.ds.web.crawler" }, excludeFilters = @Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {
-				InitalizeThreadPoolExecutor.class, WebCrawlerController.class }) )
+				InitalizeThreadPoolExecutor.class, WebCrawlerController.class, CustomWebMvcConfiguration.class }))
 public class TestConfig {
 
 }
